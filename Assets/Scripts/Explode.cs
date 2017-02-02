e07019575600a1e7eb030630d003d3799e8c5c0b
ENVY-M6\ncblx <fraugunsche@gmail.com>
2017-02-03 00:07:10 +0400

    no message

--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Explode : MonoBehaviour {
 
     public GameObject explosion;
-   // public ParticleSystem[] effects;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            GameObject clone = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
-            Destroy(clone, 1.0f);
+            Vector3 offset = new Vector3(0.0f, 2.0f, 0.0f);
+            GameObject clone = (GameObject)Instantiate(explosion, transform.position - offset, transform.rotation);
+            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 20.0f;
+            //Destroy(collision.gameObject, 0.5f);
+            Destroy(gameObject,0.5f);
+            Destroy(clone, 1.2f);
         }
     }
     
