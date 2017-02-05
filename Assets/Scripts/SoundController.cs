f8607c6ee8fc8460a69dc8a1fd4125c1ef76fa44
ENVY-M6\ncblx <fraugunsche@gmail.com>
2017-02-05 23:48:29 +0400

    no message

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+    public new AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	public void SoundOnOff()
+    {
+        if(audio.mute)
+        {
+            audio.mute = false;
+        } else
+        {
+            audio.mute = true;
+        }
+    }
+}
